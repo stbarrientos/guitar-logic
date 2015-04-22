66bf8becaabb0fbd16dccca05a3f11ba316edea3
stbarrientos <stbarrientos@gmail.com>
2015-04-21 20:13:21 -0400

    note done for now

--- lib/note.rb
+++ lib/note.rb
@@ -8,15 +8,17 @@ class Note
   end
 
   def next_note
-    new_note = Note.reference(self.name[0])
-    new_note_name = new_note[:next_note]
-    new_note = Note.reference(new_note_name)
-    Note.new(new_note_name, new_note[:prev_interval], new_note[:next_interval])
+    Note.new Note.reference(self.name[0])[:next_note]
+  end
+
+  def prev_note
+    name = self.name
+    Note.new( name[1] ? name[0] : Note.reference(name)[:prev_note])
   end
 
   def next_half_step
     return self.next_note if self.next_interval == 0.5 
-    if self.name[1] == "#"
+    if self.name[1]
       base = Note.reference(self.name[0])
       Note.new(base[:next_note])
     else
@@ -24,8 +26,21 @@ class Note
     end
   end
 
+  def prev_half_step
+    return self.prev_note if self.prev_interval == 0.5
+    if self.name[1]
+      Note.new(self.name[0])
+    else
+      Note.new( Note.reference(self.name)[:prev_note] + "#")
+    end
+  end
+
   def next_whole_step
     self.next_interval == 1 ? self.next_note : self.next_note.next_half_step
+  end
+
+  def prev_whole_step
+    self.prev_interval == 1 ? self.prev_note : self.prev_note.prev_half_step
   end
 
   def classify
@@ -40,19 +55,19 @@ class Note
   def self.reference(name)
     case name
     when "A"
-      {next_interval: 1, prev_interval: 1, next_note: "B"}
+      {next_interval: 1, prev_interval: 1, next_note: "B", prev_note: "G"}
     when "B"
-      {next_interval: 0.5, prev_interval: 1, next_note: "C"}
+      {next_interval: 0.5, prev_interval: 1, next_note: "C", prev_note: "A"}
     when "C"
-      {next_interval: 1, prev_interval: 0.5, next_note: "D"}
+      {next_interval: 1, prev_interval: 0.5, next_note: "D", prev_note: "B"}
     when "D"
-      {next_interval: 1, prev_interval: 1, next_note: "E"}
+      {next_interval: 1, prev_interval: 1, next_note: "E", prev_note: "C"}
     when "E"
-      {next_interval: 0.5, prev_interval: 1, next_note: "F"}
+      {next_interval: 0.5, prev_interval: 1, next_note: "F", prev_note: "D"}
     when "F"
-      {next_interval: 1, prev_interval: 1, next_note: "G"}
+      {next_interval: 1, prev_interval: 0.5, next_note: "G", prev_note: "E"}
     when "G"
-      {next_interval: 1, prev_interval: 1, next_note: "A"}
+      {next_interval: 1, prev_interval: 1, next_note: "A", prev_note: "F"}
     end
   end
 
--- spec/note_spec.rb
+++ spec/note_spec.rb
@@ -68,6 +68,79 @@ RSpec.describe Note, "#next_note" do
 
 end
 
+RSpec.describe Note, "#prev_note" do
+
+  context "last note" do
+
+    it "prev note before A is G" do
+      a = Note.new("A")
+      expect(a.prev_note.name).to eq("G")
+    end
+
+    it "prev note before A# is A" do
+      a_sharp = Note.new("A#")
+      expect(a_sharp.prev_note.name).to eq("A")
+    end
+
+    it "prev note before G is F" do
+      g = Note.new("G")
+      expect(g.prev_note.name).to eq("F")
+    end
+
+    it "prev note before G# is G" do
+      g_sharp = Note.new("G#")
+      expect(g_sharp.prev_note.name).to eq("G")
+    end
+
+    it "prev note before F is E" do
+      f = Note.new("F")
+      expect(f.prev_note.name).to eq("E")
+    end
+
+    it "prev note before F# is F" do
+      f_sharp = Note.new("F#")
+      expect(f_sharp.prev_note.name).to eq("F")
+    end
+
+    it "prev note before E is D" do
+      e = Note.new("E")
+      expect(e.prev_note.name).to eq("D")
+    end
+
+    it "prev note before D is C" do
+      d = Note.new("D")
+      expect(d.prev_note.name).to eq("C")
+    end
+
+    it "prev note before D# is D" do
+      d_sharp = Note.new("D#")
+      expect(d_sharp.prev_note.name).to eq("D")
+    end
+
+    it "prev note before C is B" do
+      c = Note.new("C")
+      expect(c.prev_note.name).to eq("B")
+    end
+
+    it "prev note before C# is C" do
+      c_sharp = Note.new("C#")
+      expect(c_sharp.prev_note.name).to eq("C")
+    end
+
+    it "prev note before B is A" do
+      b = Note.new("B")
+      expect(b.prev_note.name).to eq("A")
+    end
+
+    it "prev note before A# is A" do
+      a_sharp = Note.new("A#")
+      expect(a_sharp.prev_note.name).to eq("A")
+    end
+
+  end
+
+end
+
 RSpec.describe Note, "#next_whole_step" do
 
   context "could be flat or sharp" do
@@ -135,6 +208,65 @@ RSpec.describe Note, "#next_whole_step" do
 end
 
 RSpec.describe Note, "#prev_whole_step" do
+
+  context "backtracking" do
+
+    it "prev whole step before A is G" do
+      a = Note.new("A")
+      expect(a.prev_whole_step.name).to eq("G")
+    end
+
+    it "prev whole step before A# is G#" do
+      a_sharp = Note.new("A#")
+      expect(a_sharp.prev_whole_step.name).to eq("G#")
+    end
+
+    it "prev whole step before G is F" do
+      g = Note.new("G")
+      expect(g.prev_whole_step.name).to eq("F")
+    end
+
+    it "prev whole step before G# is F#" do
+      g_sharp = Note.new("G#")
+      expect(g_sharp.prev_whole_step.name).to eq("F#")
+    end
+
+    it "prev whole step before F is D#" do
+      f = Note.new("F")
+      expect(f.prev_whole_step.name).to eq("D#")
+    end
+
+    it "prev whole step before F# is E" do
+      f_sharp = Note.new("F#")
+      expect(f_sharp.prev_whole_step.name).to eq("E")
+    end
+
+    it "prev whole step before E is D" do
+      e = Note.new("E")
+      expect(e.prev_whole_step.name).to eq("D")
+    end
+
+    it "prev whole step before D# is C#" do
+      d_sharp = Note.new("D#")
+      expect(d_sharp.prev_whole_step.name).to eq("C#")
+    end
+
+    it "prev whole step before D is C" do
+      d = Note.new("D")
+      expect(d.prev_whole_step.name).to eq("C")
+    end
+
+    it "prev whole step before C is A#" do
+      c = Note.new("C")
+      expect(c.prev_whole_step.name).to eq("A#")
+    end
+
+    it "prev whole step before B is A" do
+      b = Note.new("B")
+      expect(b.prev_whole_step.name).to eq("A")
+    end
+
+  end
 
 end
 
@@ -207,5 +339,69 @@ RSpec.describe Note, "#next_half_step" do
 end
 
 RSpec.describe Note, "#prev_half_step" do
+
+  context "in reverse" do
+
+    it "prev half step of A is G#" do
+      a = Note.new("A")
+      expect(a.prev_half_step.name).to eq("G#")
+    end
+
+    it "prev half step of G# is G" do
+      g_sharp = Note.new("G#")
+      expect(g_sharp.prev_half_step.name).to eq("G")
+    end
+
+    it "prev half step of G is F#" do
+      g = Note.new("G")
+      expect(g.prev_half_step.name).to eq("F#")
+    end
+
+    it "prev half step of F# is F" do
+      f_sharp = Note.new("F#")
+      expect(f_sharp.prev_half_step.name).to eq("F")
+    end
+
+    it "prev half step of F is E" do
+      f = Note.new("F")
+      expect(f.prev_half_step.name).to eq("E")
+    end
+
+    it "prev half step of E is D#" do
+      e = Note.new("E")
+      expect(e.prev_half_step.name).to eq("D#")
+    end
+
+    it "prev half step of D# is D" do
+      d_sharp = Note.new("D#")
+      expect(d_sharp.prev_half_step.name).to eq("D")
+    end
+
+    it "prev half step of D is C#" do
+      d = Note.new("D")
+      expect(d.prev_half_step.name).to eq("C#")
+    end
+
+    it "prev half step of C# is C" do
+      c_sharp = Note.new("C#")
+      expect(c_sharp.prev_half_step.name).to eq("C")
+    end
+
+    it "prev half step of C is B" do
+      c = Note.new("C")
+      expect(c.prev_half_step.name).to eq("B")
+    end
+
+    it "prev half step of B is A#" do
+      b = Note.new("B")
+      expect(b.prev_half_step.name).to eq("A#")
+    end
+
+    it "prev half step of A# is A" do
+      a_sharp = Note.new("A#")
+      expect(a_sharp.prev_half_step.name).to eq("A")
+    end
+
+  end
 
 end

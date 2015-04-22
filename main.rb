6c64486587eeddfc8eab4fd2e69c9747aebc361b
stbarrientos <stbarrientos@gmail.com>
2015-04-21 21:54:46 -0400

    added sinatra

--- main.rb
+++ main.rb
@@ -1,1 +1,9 @@
-require_relative "./lib/*"
+require "sinatra"
+require "sinatra/reloader"
+require_relative "lib/string.rb"
+
+get "/strings/:name" do
+  @string_notes = String.new(params[:name]).show_all_frets
+  @string_name = params[:name]
+  erb :string
+end

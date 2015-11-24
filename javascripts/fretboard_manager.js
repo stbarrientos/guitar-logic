476f8a6af28d3c7cd8ed31c45fdbafb7e3cb3579
stbarrientos <stbarrientos@gmail.com>
2015-11-24 10:16:20 -0500

    added v1 of the tab writer

--- javascripts/fretboard_manager.js
+++ javascripts/fretboard_manager.js
@@ -1,5 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
 
+  var tab = {
+    1 : [],
+    2 : [],
+    3 : [],
+    4 : [],
+    5 : [],
+    6 : []
+  };
+
   // For non ruby implementation
   function drawFretboard(){
     var stringNames = ["E","B","G","D","A","E"];
@@ -8,7 +17,7 @@ document.addEventListener("DOMContentLoaded", function() {
     var frets = document.getElementsByClassName("frets")[0];
     for (var i = 0 ; i <= 21; i++){
       var existingHTML = frets.innerHTML;
-      frets.innerHTML = existingHTML + "<div class='fret'><span>" + (i + 1) + "</span></div>";
+      frets.innerHTML = existingHTML + "<div class='fret'><span>" + (i) + "</span></div>";
     };
 
     // Draw each string
@@ -19,7 +28,7 @@ document.addEventListener("DOMContentLoaded", function() {
       // Draw the notes on each string
       var allFrets = string.showAllFrets(0,21);
       for (var x = 0; x < allFrets.length; x++){
-        htmlString += "<div class='note'><div>" + allFrets[x] + "</div></div>";
+        htmlString += "<div class='note' data-fret='" + x + "'' data-string='" + (i + 1) + "'><div>" + allFrets[x] + "</div></div>";
       };
 
       var fretboard = document.getElementById("fretboard");
@@ -79,4 +88,59 @@ document.addEventListener("DOMContentLoaded", function() {
     };
   });
 
+  // Add highlighted frets to tab
+  document.getElementById("add-to-tab").addEventListener("click", function (){
+    var highlighted = document.getElementsByClassName("highlighted");
+
+    // Go through all six strings and update our tab object
+    for (var i = 1; i <= 6; i++){
+
+      // If we have a note on that string (or notes), add them to the object
+      var notesOnString = []
+      for (var x = 0; x < highlighted.length; x++){
+
+        // If this highlighted note is on the same string that we are currently on
+        var currentNote = highlighted[x].parentNode;
+        if (currentNote.getAttribute("data-string") == i){
+          notesOnString.push(currentNote.getAttribute("data-fret"));
+        };
+      };
+
+      // If no highlighted notes were found on that string, add an empty mark
+      if (notesOnString.length === 0){
+        notesOnString.push("-")
+      };
+
+      // Add each item of notesOnString to the object array
+      for (var y = 0; y < notesOnString.length; y++){
+        tab[i].push(notesOnString[y]);
+      };
+    };
+
+    // Write the tab object to the page
+    writeTabToPage();
+  });
+
+  function writeTabToPage(){
+    var textarea = document.getElementsByTagName("textarea")[0];
+
+    // Clear the textarea
+    textarea.value = "";
+    console.log(tab)
+
+    // Go through each string
+    for (var i = 1; i <= 6; i++){
+
+      // Access the notes for this string in the tabs object
+      var notesArray = tab[i];
+
+      for (var x = 0; x < notesArray.length; x++){
+        textarea.value += notesArray[x] + " - ";
+      };
+
+      textarea.value += "\n";
+    };
+  };
 });
+
+
